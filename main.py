6f6b28111f8e4cf6a781f559401231a1af26ecba
alenaodinets <alena.odinecz2013@yandex.ru>
2024-03-01 19:56:53 +0400

    create main file

--- main.py
+++ main.py
@@ -1,16 +1,68 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from src.user_inter import UserInteraction, StopUserProgram
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+
+    while True:
+        print('Какую професcию ты ищешь?')
+        prof = input()
+        if not prof:
+            print('Ну же, выбери профессию!')
+            continue
+
+        print('\nВ каком именно городе хотел бы работать?')
+        print('Если нет конкретного города, жми Enter')
+        town = input()
+        if not town:
+            town = None
+
+        while True:
+            print('\nCколько вакансий тебе показать?')
+            print('Выведется топ по зарплатам\n')
+            vacancies = input()
+            if not UserInteraction.validate_int(vacancies):
+                continue
+
+            print('\nВыбирай любую страницу каталога\n')
+            page = input()
+            if not UserInteraction.validate_int(page):
+                continue
+
+            print('\nПереведем любую иностранную валюту в рубли?\n')
+            UserInteraction.user_notification_choise()
+            convert = input()
+            if not convert:
+                convert = False
+            else:
+                convert = True
+
+            print('\nОбрабатываем, ожидайте')
+
+            try:
+                UserInteraction.save_to_file(name=prof, page=page, per_page=vacancies, convert_to_RUB=convert,
+                                             town=town)
+
+            except StopUserProgram:
+                break
+
+            print('\nПоищем ещё?')
+            print('Изменим  страницу и количество вакансий?')
+            UserInteraction.user_notification_choise()
+
+            if not input():
+                break
+            continue
+
+        print('\nПосморим другую профессию?')
+        print('Посмотрим другой город?')
+        UserInteraction.user_notification_choise()
+
+        if not input():
+            print('\nМы надеемся, что ты нашел, что искал! А если нет - будем ждать снова) До встречи!')
+            break
+        continue
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
